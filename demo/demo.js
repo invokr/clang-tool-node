05e42324582486d5f2f19f140a41afb4e42ed219
Robin <me@invokr.org>
2015-02-25 19:33:21 +0000

    Added demo

---
+++ demo/demo.js
@@ -0,0 +1,17 @@
+// Get the native module and initialize the library
+var clang_autocomplete = require("../build/Release/clang_tool.node");
+var obj = new clang_autocomplete.object;
+
+// Set your projects compiler flags
+obj.setArgs(["-I/usr/include", "-I/usr/local/include"]);
+
+// Add files to the index, an absolute path is required
+// If the path is relative, reparses will break certain functions
+var path = require('path');
+var file = path.resolve('./demo.js');
+obj.indexTouch(file);
+
+/// You can now call any of the internal functions
+console.log(
+    obj.indexDiagnose(file)
+);
