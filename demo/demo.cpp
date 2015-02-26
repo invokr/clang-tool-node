cc44860eaa267ddf4e2652e96947e73c6a005e63
Robin <me@invokr.org>
2015-02-26 18:14:03 +0000

    Added demo.cpp

---
+++ demo/demo.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <clang-c/Index.h>
+#include <string>
+#include "asd"
+
+int i;
+std::string x;
+
+struct m {
+    int f;
+};
+
+class class__ {
+    public:
+        class__(int x, int y);
+        int j;
+};
+
+
+
+int myFunc(int i, int j) {
+
+}
+
+void a() {
+    std::unordered_map<int, int> b;
+    b.
+}
